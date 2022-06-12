fcefb1d642b7c2fb16a123e4787bbec1b7f94d20
crizmo <crizmo595@gmail.com>
2022-06-12 15:15:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
-import Card from '../src/lib/card.js';
+import io from 'socket.io-client'
+import { useState } from 'react';
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+  const [cards, setCards] = useState([])
+
+  socket.on('message', function (data) {
+    // console.log(data)
+
+    let datainfo = [{
+      id: data.stuff.id,
+      name: data.stuff.name,
+      state: data.stuff.state,
+      type: data.stuff.type,
+      details: data.stuff.details,
+    }]
+
+    setCards(datainfo)
+  })
+
+  
+  const sendUser = () => {
+    const userid = document.getElementById('user-id').value
+    // console.log(userid)
+    socket.emit('user', { userid: userid })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Card />
+        <h1>DCEZ Client</h1>
+
+        <input type="text" id='user-id' placeholder="Enter your user id" defaultValue="784141856426033233" />
+        <button onClick={sendUser}>Send User</button>
+        
+        {cards.map((card, index) => {
+          return (
+            <div key={index}>
+              <h2>{card.name}</h2>
+              <p>{card.state}</p>
+              <p>{card.id}</p>
+              <p>{card.type}</p>
+              <p>{card.details}</p>
+            </div>
+          )
+        })}
       </header>
     </div>
   );
