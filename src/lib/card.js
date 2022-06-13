0a3bc371b595fd2863c6ab7b081439805201ddce
crizmo <crizmo595@gmail.com>
2022-06-14 00:16:46 +0530

    Update card.js

--- src/lib/card.js
+++ src/lib/card.js
@@ -1,62 +1,68 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import '../App.css';
+import io from 'socket.io-client'
+import { useState } from 'react';
+const socket = io.connect("http://localhost:3001")
 
-export default class card extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            card: "getting card"
-        }
+function Card() {
+    const [cards, setCards] = useState([])
+
+    socket.on('message', function (data) {
+        // console.log(data.card)
+
+        let datainfo = [{
+            id: data.stuff.id,
+            name: data.stuff.name,
+            state: data.stuff.state,
+            type: data.stuff.type,
+            details: data.stuff.details,
+
+            svg: data.card,
+            basecard: data.baseimg
+        }]
+
+        setCards(datainfo)
+    
+    })
+
+    
+    const sendUser = () => {
+        const userid = document.getElementById('user-id').value
+        // console.log(userid)
+        socket.emit('user', { userid: userid })
     }
 
-    updateCard = () => {
-        axios.get('/card')
-        .then(res => {
-            this.setState({
-                cards: res.data.name
-            })
-        }
-    )}
-
-    handleButtonClick = () => {
-        
-        axios.get('/card').then(response => {
-            // console.log(response.data);
-            this.setState({
-                cardname: response.data.name,
-                cardid: response.data.id,
-                cardtype: response.data.type,
-                carddetails: response.data.details,
-                cardstate: response.data.state
-            })
-        }) 
-
-        axios.get(`/card/${document.getElementById('discordid').value}`).then(response => {
-            // console.log(response.data);
-            this.setState({
-                cardname: response.data.name,
-                cardid: response.data.id,
-                cardtype: response.data.type,
-                carddetails: response.data.details,
-                cardstate: response.data.state,
-            })
+    const sendSvg = () => {
+        // eslint-disable-next-line
+        cards.map((card) => {
+            // console.log(card.svg)
+            socket.emit('svg', { svg: card.svg })
         })
     }
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleButtonClick}>Get Card</button>
-                <input type="text" id="discordid" />
-                <p>{this.state.cardname}</p>
-                <p>{this.state.cardid}</p>
-                <p>{this.state.cardtype}</p>
-                <p>{this.state.carddetails}</p>
-                <p>{this.state.cardstate}</p>
 
-                <button onClick={this.updateCard}>Update Card</button>
-                <p>{this.state.cards}</p>
+    return (
+        <div className="App">
+            <div className="main">
+                <input type="text" id='user-id' placeholder="Enter your user id" defaultValue="784141856426033233" />
+                <button onClick={sendUser}>Send User</button>
+                <button onClick={sendSvg}>Send SVG</button>
+
+                {cards.map((card, index) => {
+                    return (
+                        <div key={index} className="card">
+                            <h2>{card.name}</h2>
+                            <p>{card.details}</p>
+                            <p>{card.state}</p>
+                            <p>{card.id}</p>
+                            <p className='type'>{card.type}</p>
+                            <img src={`data:image/svg+xml;base64,${card.basecard}`} alt="" />
+                            <img id='svg-input' src={`data:image/svg+xml;svg+sml,${card.svg}`} alt="" />
+                        </div>
+                    )
+                })}
             </div>
-        )
-    }
+        </div>
+    );
 }
+
+export default Card;
