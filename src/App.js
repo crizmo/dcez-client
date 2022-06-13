0ae337ab6f563bf2492478114884ed23d57dd2b4
crizmo <crizmo595@gmail.com>
2022-06-14 00:16:35 +0530

    main routing

--- src/App.js
+++ src/App.js
@@ -1,53 +1,14 @@
-import './App.css';
-import io from 'socket.io-client'
-import { useState } from 'react';
-const socket = io.connect("http://localhost:3001")
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Card from "./lib/card";
 
 function App() {
-  const [cards, setCards] = useState([])
-
-  socket.on('message', function (data) {
-    // console.log(data)
-
-    let datainfo = [{
-      id: data.stuff.id,
-      name: data.stuff.name,
-      state: data.stuff.state,
-      type: data.stuff.type,
-      details: data.stuff.details,
-
-      card: data.card,
-      basecard: data.baseimg
-    }]
-
-    setCards(datainfo)
-  })
-
-  const sendUser = () => {
-    const userid = document.getElementById('user-id').value
-    // console.log(userid)
-    socket.emit('user', { userid: userid })
-  }
-
   return (
     <div className="App">
-      <div className="main">
-        <input type="text" id='user-id' placeholder="Enter your user id" defaultValue="784141856426033233" />
-        <button onClick={sendUser}>Send User</button>
-        
-        {cards.map((card, index) => {
-          return (
-            <div key={index} className="card">
-              <h2>{card.name}</h2>
-              <p>{card.details}</p>
-              <p>{card.state}</p>
-              <p>{card.id}</p>
-              <p className='type'>{card.type}</p>
-              <img src={`data:image/svg+xml;base64,${card.basecard}`} alt=""/>
-            </div>
-          )
-        })}
-      </div>
+      <Routes>
+        <Route path="/" element={ <Card />} />
+      </Routes>
     </div>
   );
 }
