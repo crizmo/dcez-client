6a33901942d7af86659f6c8a155e3bc7704c6d44
crizmo <crizmo595@gmail.com>
2022-06-15 09:13:54 +0530

    Update card.js

--- src/lib/card.js
+++ src/lib/card.js
@@ -8,30 +8,28 @@ function Card() {
 
     socket.on('message', function (data) {
         // console.log(data.card)
-
+        
         let datainfo = [{
             id: data.stuff.id,
             name: data.stuff.name,
             state: data.stuff.state,
             type: data.stuff.type,
             details: data.stuff.details,
-
+            
             svg: data.card,
             basecard: data.baseimg
         }]
-
-        setCards(datainfo)
-
-        // load the svg into the div
+        
         let svgdiv = document.getElementById('svg')
         svgdiv.innerHTML = data.card
+
+        setCards(datainfo)
     
     })
 
     
     const sendUser = () => {
         const userid = document.getElementById('user-id').value
-        // console.log(userid)
         socket.emit('user', { userid: userid })
     }
 
@@ -40,27 +38,21 @@ function Card() {
             <div className="main">
                 <input type="text" id='user-id' placeholder="Enter your user id" defaultValue="784141856426033233" />
                 <button onClick={sendUser}>Send User</button>
-                {/* {cards.map((card, index) => {
-                    return (
-                        <div key={index} className="card">
-                            <h2>{card.name}</h2>
-                            <p>{card.details}</p>
-                            <p>{card.state}</p>
-                            <p>{card.id}</p>
-                            <p className='type'>{card.type}</p>
-                            <img src={`data:image/svg+xml;base64,${card.basecard}`} alt="" />
-                        </div>
-                    )
-                })} */}
-                <div className="card">
-                    <h2>{cards[0].name}</h2>
-                    <p>{cards[0].details}</p>
-                    <p>{cards[0].state}</p>
-                    <p>{cards[0].id}</p>
-                    <p className='type'>{cards[0].type}</p>
+                <div className='card'>
+                    {cards.map((card, index) => {
+                        return (
+                            <div key={index}>
+                                <h2>{card.name}</h2>
+                                <p>{card.details}</p>
+                                <p>{card.state}</p>
+                                <p>{card.id}</p>
+                                <p className='type'>{card.type}</p>
+                                {/* <img src={`data:image/svg+xml;base64,${card.basecard}`} alt="" /> */}
+                            </div>
+                        )
+                    })}
                     <div id='svg' className='svgimage'></div>
                 </div>
-                {/* <div id='svg' className='svgimage'></div> */}
             </div>
         </div>
     );
