b4722313f92af38213878d1ef370f405e0b0071b
crizmo <crizmo595@gmail.com>
2022-06-19 22:57:09 +0530

    Update card.js

--- src/lib/card.js
+++ src/lib/card.js
@@ -32,13 +32,18 @@ function Card() {
         const userid = document.getElementById('user-id').value
         const about = document.getElementById('about').value
         const banner = document.getElementById('banner').value
+
+        const large_image = document.getElementById('large-image').value
+        const small_image = document.getElementById('small-image').value
         if (!userid) {
             alert('Please enter a user id')
         } else {
             socket.emit('user', { 
                 userid: userid,
                 about: about,
-                banner: banner
+                banner: banner,
+                large_image: large_image,
+                small_image: small_image
             })
         }
     }
@@ -46,9 +51,11 @@ function Card() {
     return (
         <div className="App">
             <div className="main">
-                <input type="text" id='user-id' placeholder="Enter your user id" />
-                <input type="text" id='about' placeholder="Enter your about" defaultValue="Itz me !" />
-                <input type="text" id='banner' placeholder='Enter your banner image url' defaultValue="https://wallpapercave.com/wp/wp4771870.jpg" />
+                <input className='inp' type="text" id='user-id' placeholder="Enter your user id" />
+                <input className='inp' type="text" id='about' placeholder="Enter your about" defaultValue="Itz me !" />
+                <input className='inp' type="text" id='banner' placeholder='Enter your banner image url' defaultValue="https://wallpapercave.com/wp/wp4771870.jpg" />
+                <input className='large' type="text" id='large-image' placeholder='Enter your large image url' defaultValue="https://cdn.discordapp.com/attachments/952764969638834228/979995425874513960/Criz.png" />
+                <input className='small' type="text" id='small-image' placeholder='Enter your small image url' defaultValue="https://cdn.discordapp.com/attachments/952764969638834228/979994810012295208/Criz.png" />
                 <button onClick={sendUser}>Send User</button>
                 <div className='card'>
                     {cards.map((card, index) => {
