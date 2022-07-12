b475fff4de6e59b6bf0f5b3464f2dbdd4b4b1b1b
crizmo <crizmo595@gmail.com>
2022-07-13 00:16:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Card from "./lib/card";
-import Doc from "./lib/doc";
+// import Doc from "./lib/doc";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Doc /> } />
-        <Route path="/card" element={ <Card /> } />
+        {/* <Route path="/" element={ <Doc /> } />
+        <Route path="/card" element={ <Card /> } /> */}
+        <Route path="/" element={ <Card/> } />
       </Routes>
     </div>
   );
