715739002c80345e1c7801a5762962a0f433824b
crizmo <crizmo595@gmail.com>
2022-07-13 00:13:01 +0530

    Revert "Delete App.js"
    This reverts commit 76a033014f9ab45a1b98f071fe4d91d09e17eb59.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Card from "./lib/card";
+import Doc from "./lib/doc";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <Doc /> } />
+        <Route path="/card" element={ <Card /> } />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
