9e624d05eaa01c1a0bfec99694c88692c56eede2
crizmo <crizmo595@gmail.com>
2022-08-24 22:22:48 +0530

    🔥 : react 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
 );
