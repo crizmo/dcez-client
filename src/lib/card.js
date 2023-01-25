f5de6843b726f657e1f73c0658050b5e252e2b6e
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-01-25 23:53:41 +0530

    🔮 : card page ui changes

--- src/lib/card.js
+++ src/lib/card.js
@@ -1,130 +1,200 @@
 import '../App.css';
-import io from 'socket.io-client'
-import { useState } from 'react';
-const socket = io.connect("https://breeze-api.kurizu.repl.co/") // http://localhost:3001/ // https://breeze-api.kurizu.repl.co/
+import {
+    Box,
+    Button,
+    TextField,
+    useMediaQuery,
+    Typography,
+    useTheme
+} from "@mui/material";
+import { inputLabelClasses } from "@mui/material/InputLabel";
 
 function Card() {
-    const [cards, setCards] = useState([])
-    const [notInGuild, setNotInGuild] = useState([])
 
-    socket.on('message', function (data) {
-        // console.log(data.card)
-
-        let user = document.getElementById('user-id').value
-        let api = `https://breeze-api.kurizu.repl.co/api/card/${user}`
-        
-        let about = document.getElementById('about').value
-        let banner = document.getElementById('banner').value
-        let large_image = document.getElementById('large-image').value
-        let small_image = document.getElementById('small-image').value
-
-        let api_link = `${api}?about=${about}&banner=${banner}&large_image=${large_image}&small_image=${small_image}`
-        const link_div = document.getElementById('link-div')
-        link_div.innerHTML = `<a href="${api_link}" target="_blank">Static API Card</a>`
-
-        let datainfo = [{
-            id: data.stuff.id,
-            name: data.stuff.name,
-            state: data.stuff.state,
-            type: data.stuff.type,
-            details: data.stuff.details,
-            
-            svg: data.card,
-            apiimg: api
-        }]
-        
-        let svgdiv = document.getElementById('svg')
-        svgdiv.innerHTML = data.card
-
-        setCards(datainfo)
-    
-    })
-
-    socket.on('not-in-server', function (data) {
-
-        let not_in_guild = [{
-            user: data.user_id
-        }]
-
-        setNotInGuild(not_in_guild)
-        let svgdiv = document.getElementById('svg')
-        svgdiv.innerHTML = ``
-    })
-
-    socket.on('no-activity', function (data) {
-        let svgdiv = document.getElementById('svg')
-        svgdiv.innerHTML = data.card
-    })
-
-    
-    const sendUser = () => {
+    const loadapi = () => {
         const userid = document.getElementById('user-id').value
-        const about = document.getElementById('about').value
-        const banner = document.getElementById('banner').value
-
-        const large_image = document.getElementById('large-image').value
-        const small_image = document.getElementById('small-image').value
-        if (!userid) {
-            alert('Please enter a user id')
-        } else {
-            socket.emit('user', { 
-                user: userid, 
-                about: about,
-                banner: banner,
-                large_image: large_image,
-                small_image: small_image
-            })
-
-            // console.log("User id : " + userid)
-        }
+        const api = `http://localhost:3001/api/card/${userid}`
+        const svgdiv = document.getElementById('api')
+        svgdiv.innerHTML = `<img src="${api}" alt="">`
     }
-
-    // const loadapi = () => {
-    //     const userid = document.getElementById('user-id').value
-    //     const api = `https://breeze-api.kurizu.repl.co/api/card/${userid}`
-    //     const svgdiv = document.getElementById('api')
-    //     svgdiv.innerHTML = `<img src="${api}" alt="">`
-    // }
 
     return (
         <div className="App">
             <div className="main">
-                <div className="inputs">
-                    <h2 className='site'>Breeze</h2>
-                    <input className='inp-id' type="text" id='user-id' placeholder="Enter your user id" />
-                    <input className='inp-about' type="text" id='about' placeholder="Enter your about" defaultValue="Itz me !" />
-                    <input className='inp-banner' type="text" id='banner' placeholder='Enter your banner image url'/>
-                    <input className='large' type="text" id='large-image' placeholder='Enter your large image url' />
-                    <input className='small' type="text" id='small-image' placeholder='Enter your small image url' />
-                    <button onClick={sendUser} className='sendbtn'>Send User</button>
-                </div>
-                <div className='card'>
-                    {cards.map((card, index) => {
-                        return (
-                            <div key={index}>
-                                    {/* <h2 className='card-name'>{card.name}</h2>
-                                    <p className='card-state'>{card.state}</p>
-                                    <p className='card-details'>{card.details}</p>
-                                    <p className='card-type'>{card.type}</p> */}
-                                    {/* <button onClick={loadapi}>Load API</button> */}
-                            </div>
-                        )
-                    })}
-                    {notInGuild.map((user, index) => {
-                        setTimeout(() => {
-                            setNotInGuild([])
-                            setCards([])
-                        } , 5000)
-                        return (
-                            <p key={index}>
-                                User with id {user.user} is not in the server
-                            </p>
-                        )
-                    })}
-                    <div id='svg' className='svgimage'></div>
-                    {/* <div id='api' className='apiimage'></div> */}
-                    <div id='link-div' className='link-div'></div>
-                </div>
+                <header className="header">
+                    <h2 className="title">Breeze API</h2>
+                </header>
+                <Box
+                    display="grid"
+                    gap="10px"
+                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                    sx={{
+                        width: '100%',
+                        maxWidth: 500,
+                        mb: 2,
+                    }}
+                    // align items center
+                    className="inputs"
+                >
+                    <TextField
+                        id="user-id"
+                        className='user-id'
+                        label="User ID"
+                        variant="outlined"
+                        sx={{
+                            width: '100%',
+                            maxWidth: 250,
+                            mb: 2,
+                            gridColumn: "span 2",
+                            input: {
+                                color: "#99aab5"
+                            }   
+                        }}
+                        InputLabelProps={{
+                            sx: {
+                                color: "rgb(77, 154, 255)",
+                                [`&.${inputLabelClasses.shrink}`]: {
+                                    color: "rgb(77, 154, 255)"
+                                }
+                            }
+                        }}
+                    />
+                    <TextField
+                        id="about-me"
+                        className='about-me'
+                        label="About Me"
+                        variant="outlined"
+                        sx={{
+                            width: '100%',
+                            maxWidth: 250,
+                            mb: 2,
+                            gridColumn: "span 2",
+                            input: {
+                                color: "#99aab5"
+                            } 
+                        }}
+                        InputLabelProps={{
+                            sx: {
+                                color: "rgb(77, 154, 255)",
+                                [`&.${inputLabelClasses.shrink}`]: {
+                                    color: "rgb(77, 154, 255)"
+                                }
+                            }
+                        }}
+                    />
+
+                    <TextField
+                        id="banner-url"
+                        className='banner-url'
+                        label="Banner URL"
+                        variant="outlined"
+                        sx={{
+                            width: '100%',
+                            maxWidth: 500,
+                            mb: 2,
+                            gridColumn: "span 4",
+                            input: {
+                                color: "#99aab5"
+                            } 
+                        }}
+                        InputLabelProps={{
+                            sx: {
+                                color: "rgb(77, 154, 255)",
+                                [`&.${inputLabelClasses.shrink}`]: {
+                                    color: "rgb(77, 154, 255)"
+                                }
+                            }
+                        }}
+                    />
+                    <TextField
+                        id="large-image-url"
+                        className='large-image-url'
+                        label="Large Image URL"
+                        variant="outlined"
+                        sx={{
+                            width: '100%',
+                            maxWidth: 500,
+                            mb: 2,
+                            gridColumn: "span 4",
+                            input: {
+                                color: "#99aab5"
+                            } 
+                        }}
+                        InputLabelProps={{
+                            sx: {
+                                color: "rgb(77, 154, 255)",
+                                [`&.${inputLabelClasses.shrink}`]: {
+                                    color: "rgb(77, 154, 255)"
+                                }
+                            }
+                        }}
+                    />
+                    <TextField
+                        id="small-image-url"
+                        className='small-image-url'
+                        label="Small Image URL"
+                        variant="outlined"
+                        sx={{
+                            width: '100%',
+                            maxWidth: 500,
+                            mb: 2,
+                            gridColumn: "span 4",
+                            input: {
+                                color: "#99aab5"
+                            } 
+                        }}
+                        InputLabelProps={{
+                            sx: {
+                                color: "rgb(77, 154, 255)",
+                                [`&.${inputLabelClasses.shrink}`]: {
+                                    color: "rgb(77, 154, 255)"
+                                }
+                            }
+                        }}
+                    />
+                    <TextField
+                        id="hex-code"
+                        className='hex-code'
+                        label="Hex Code"
+                        variant="outlined"
+                        sx={{
+                            width: '100%',
+                            maxWidth: 500,
+                            mb: 2,
+                            gridColumn: "span 1",
+                            input: {
+                                color: "#99aab5"
+                            } 
+                        }}
+                        InputLabelProps={{
+                            sx: {
+                                color: "rgb(77, 154, 255)",
+                                [`&.${inputLabelClasses.shrink}`]: {
+                                    color: "rgb(77, 154, 255)"
+                                }
+                            }
+                        }}
+                    />
+                    <Button
+                        variant="contained"
+                        onClick={loadapi}
+                        sx={{ 
+                            width: '100%', 
+                            height: '100%',
+                            maxWidth: 500, 
+                            maxHeight: 55,
+                            mb: 2, 
+                            backgroundColor: 'rgb(77, 154, 255)', 
+                            alignSelf: 'center', 
+                            gridColumn: "span 3",
+                            color: "#23272a",
+                        }}
+                    >
+                        Load API
+                    </Button>
+                    <div id="api" className="api" />
+                </Box>
+
             </div>
         </div>
     );
