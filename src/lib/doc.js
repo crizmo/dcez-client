d602c387252abc08b767ac106724a056b4825e5d
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-01-26 16:40:28 +0530

    🚧 : security risks

--- src/lib/doc.js
+++ src/lib/doc.js
@@ -14,7 +14,7 @@ function Doc() {
                     <p className="doc-text">
                         Breeze helps you display your discord activity on your website / anywhere you want using the api or the client. <br />
                         <br />
-                        To get started, you need to join the <a href="https://discord.gg/Ecy6WpEZsD" target="_blank" rel='noreferrer'>Breeze Discord</a> <br/>
+                        To get started, you need to join the <a href="https://discord.gg/Ecy6WpEZsD" target="_blank" rel="noopener noreferrer">Breeze Discord</a> <br/>
                         Thats all you need to do. A discord bot is used to fetch your activity data.<br />
                         <br />
                         Once you have joined the server, you can use the client or the api to display your activity.
@@ -26,15 +26,15 @@ function Doc() {
                         Endpoints of the api: <br />
                         <br />
                         Cards: <br />
-                        <a href="https://breeze-api.kurizu.repl.co/api/card" target="_blank" rel='noreferrer'>/api/card/:id</a> <br />
-                        <a href="https://breeze-api.kurizu.repl.co/api/compact" target="_blank" rel='noreferrer'>/api/compact/:id</a> <br /> <br />
+                        <a href="https://breeze-api.kurizu.repl.co/api/card" target="_blank" rel="noopener noreferrer">/api/card/:id</a> <br />
+                        <a href="https://breeze-api.kurizu.repl.co/api/compact" target="_blank" rel="noopener noreferrer">/api/compact/:id</a> <br /> <br />
                         Replace :id with your user id <br />
                         <br />
-                        More info on Github: <a href="https://github.com/crizmo/breeze" target="_blank" rel='noreferrer'>Breeze Github</a>
+                        More info on Github: <a href="https://github.com/crizmo/breeze" target="_blank" rel="noopener noreferrer">Breeze Github</a>
                     </p>
                     <h2 className="doc-title">Client</h2>
                     <p className="doc-text">
-                        Client site of breeze is available on <a href="https://breeze-stats.netlify.app/card" rel='noreferrer'>https://breeze-stats.netlify.app/card</a> <br />
+                        Client site of breeze is available on <a href="https://breeze-stats.netlify.app/card" rel="noopener noreferrer">https://breeze-stats.netlify.app/card</a> <br />
                         <br />
                         Use it to get the right customization for your card. <br />
                     </p>
